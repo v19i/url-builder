07603ea9dd402401e1f5c74387ef8ea5f089b129
Valerio Francescangeli <franz899@gmail.com>
2020-06-11 05:41:39 +0100

    URLBuilder with search params support

---
+++ src/url-builder.test.js
@@ -0,0 +1,63 @@
+require("mocha");
+const chai = require("chai");
+chai.should();
+const expect = chai.expect;
+const URLBuilder = require("./url-builder.js");
+
+describe("URLBuilder", () => {
+  describe("constructor", () => {
+    it("should require a base path", () => {
+      expect(() => new URLBuilder()).to.throw("baseURL is requried");
+    });
+  });
+
+  it("should return a url", () => {
+    const b = new URLBuilder("https://a.valid.url/with/a/long/path");
+
+    b.URL.should.equal("https://a.valid.url/with/a/long/path");
+  });
+
+  describe("search params", () => {
+    it("should allow to add a search params", () => {
+      const b = new URLBuilder("https://a.valid.url/");
+      b.search.add("key", "value");
+
+      b.URL.should.equal("https://a.valid.url/?key=value");
+    });
+
+    it("should allow to add multiple search param", () => {
+      const b = new URLBuilder("https://a.valid.url/");
+      b.search.add("key", "value");
+      b.search.add("key2", "value2");
+
+      b.URL.should.equal("https://a.valid.url/?key=value&key2=value2");
+    });
+
+    it("should allow to edit an existing search param", () => {
+      const b = new URLBuilder("https://a.valid.url/");
+      b.search.add("key", "value");
+      b.search.add("key", "new_value");
+
+      b.URL.should.equal("https://a.valid.url/?key=new_value");
+    });
+
+    it("should allow to remove search params", () => {
+      const b = new URLBuilder("https://a.valid.url/");
+      b.search.add("key", "value");
+      b.search.add("key2", "value2");
+      b.search.del("key2");
+
+      b.URL.should.equal("https://a.valid.url/?key=value");
+    });
+
+    it("should encode search special characters", () => {
+      const b = new URLBuilder("https://a.valid.url/");
+      b.search.add("key", "value");
+      b.search.add("email", "an@email.address");
+
+      b.URL.should.equal(
+        "https://a.valid.url/?key=value&email=an%40email.address"
+      );
+    });
+  });
+});
