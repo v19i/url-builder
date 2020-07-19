d328c5a6e64e28ed84364a98d494e602622cfeb3
Valerio Francescangeli <franz899@gmail.com>
2020-07-19 16:03:51 +0100

    Updates dev packages

--- src/url-builder.js
+++ src/url-builder.js
@@ -1,18 +1,16 @@
 class URLBuilder {
-  #baseURL;
-  
   constructor(baseURL) {
     if (isNotAString(baseURL)) {
       throw new Error("baseURL is requried");
     }
 
-    this.#baseURL = baseURL;
+    this.baseURL = baseURL;
     this.search = new Params();
   }
 
   get URL() {
     const search = this.search.format();
-    return `${this.#baseURL}${search}`;
+    return `${this.baseURL}${search}`;
   }
 }
 
@@ -23,20 +21,18 @@ function isNotAString(x) {
 }
 
 class Params {
-  #params
-
   constructor() {
-    this.#params = {};
+    this.params = {};
   }
 
   format() {
-    const keys = Object.keys(this.#params);
+    const keys = Object.keys(this.params);
     if (keys.length === 0) {
       return "";
     }
 
     const params = keys.map((key, i) => {
-      const value = encodeURIComponent(this.#params[key]);
+      const value = encodeURIComponent(this.params[key]);
       return `${key}=${value}`;
     });
 
@@ -44,10 +40,10 @@ class Params {
   }
 
   set(key, value) {
-    this.#params[key] = value;
+    this.params[key] = value;
   }
 
   del(key) {
-    delete this.#params[key];
+    delete this.params[key];
   }
 }
